3438f046d51ff3eb51e7fd3d87b0bb1b87dbd3ba
bracketeer <brent.stradling@gmail.com>
2017-04-29 14:14:58 -0500

    added css variables and some javascript to change site colors on pallete select from navigation sidebar

--- js/main.js
+++ js/main.js
@@ -1,1 +1,24 @@
 console.log("dope Shoes");
+$('#pallete2').click(function(){
+
+  $('#pallete1').removeClass('bg').addClass('fg');
+  $('#pallete2').removeClass('fg').addClass('bg');
+});
+$('#pallete1').click(function(){
+  $('#pallete1').addClass('bg').removeClass('fg');
+  $('#pallete2').addClass('fg').removeClass('bg');
+});
+function swapColor1(){
+  document.getElementsByClassName('about')[0].style.background = 'var(--bgcolor)'
+  document.getElementById('h1').style.color = 'var(--fgcolor)'
+}
+function swapColor2(){
+    document.getElementById('h1').style.color = 'var(--bgcolor)'
+  document.getElementsByClassName('about')[0].style.background = 'var(--fgcolor)'
+}
+// function swapColor1(){
+//   document.getElementById('about').style.background = 'var(--bgcolor)'
+// }
+// function swapColor2(){
+//   document.getElementById('about').style.background = 'var(--fgcolor)'
+// }
