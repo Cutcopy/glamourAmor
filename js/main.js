1f9587b41884af1e2c730f987891b88d52e9d47c
bracketeer <brent.stradling@gmail.com>
2017-04-29 18:20:43 -0500

    added color pickers

--- js/main.js
+++ js/main.js
@@ -1,24 +1,47 @@
 console.log("dope Shoes");
-$('#pallete2').click(function(){
 
-  $('#pallete1').removeClass('bg').addClass('fg');
-  $('#pallete2').removeClass('fg').addClass('bg');
+//shortens some of the js funtions, because laziness
+var byId = function ( id ){ return document.getElementById( id )}
+var byClass = function ( classname ){ return document.getElementsByClassName( classname )}
+
+//color picker
+var colorInput1 = document.querySelector('input#pallete1');
+var colorInput2 = document.querySelector('input#pallete2');
+
+//main Color Selector
+colorInput1.addEventListener('input', function(){
+  var bg = colorInput1.value
+  var elems = byClass('sitecolor1');
+  for (var i=0;i<elems.length;i+=1){
+    elems[i].style.background = bg;
+  }
 });
-$('#pallete1').click(function(){
-  $('#pallete1').addClass('bg').removeClass('fg');
-  $('#pallete2').addClass('fg').removeClass('bg');
+
+//forground color selector
+colorInput2.addEventListener('input', function(){
+  var fg = colorInput2.value
+  var elems = byClass('sitecolor2');
+  for (var i=0;i<elems.length;i+=1){
+    elems[i].style.color = fg;
+  }
 });
-function swapColor1(){
-  document.getElementsByClassName('about')[0].style.background = 'var(--bgcolor)'
-  document.getElementById('h1').style.color = 'var(--fgcolor)'
-}
-function swapColor2(){
-    document.getElementById('h1').style.color = 'var(--bgcolor)'
-  document.getElementsByClassName('about')[0].style.background = 'var(--fgcolor)'
-}
-// function swapColor1(){
-//   document.getElementById('about').style.background = 'var(--bgcolor)'
-// }
-// function swapColor2(){
-//   document.getElementById('about').style.background = 'var(--fgcolor)'
-// }
+
+//toggles inbetween color choices
+function swapColor(){
+  //assign colors into variables from color input
+  var bg = colorInput1.value;
+  var fg = colorInput2.value;
+
+  //swap colors in input fields
+  byId('pallete1').value = fg;
+  byId('pallete2').value = bg;
+  //run through all classes and swap colors on page
+  var elems = byClass('sitecolor1');
+  for (var i=0;i<elems.length;i+=1){
+    elems[i].style.background = fg;
+    }
+    var elems = byClass('sitecolor2');
+    for (var i=0;i<elems.length;i+=1){
+      elems[i].style.color = bg;
+    }
+};
